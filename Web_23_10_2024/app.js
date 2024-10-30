ebd4d06418ab1b295423e923373ae9f05106deef
tovandat08052005 <165648707+tovandat08052005@users.noreply.github.com>
2024-10-30 07:50:20 +0700

    Update app.js

--- Web_23_10_2024/app.js
+++ Web_23_10_2024/app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.get('/user', (req, res) => {
+    res.json([
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Smith' }
+    ]);
+});
 

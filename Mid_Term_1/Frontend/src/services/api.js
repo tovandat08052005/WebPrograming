12e41b2f0bdbc83fee2b9739a8c960ffc8174c52
tovandat08052005 <165648707+tovandat08052005@users.noreply.github.com>
2024-11-10 22:17:20 +0700

    Update api.js

--- Mid_Term_1/Frontend/src/services/api.js
+++ Mid_Term_1/Frontend/src/services/api.js
@@ -1,1 +1,12 @@
+// src/services/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api', 
+});
+
+export const getTodos = () => api.get('/todos');
+export const createTodo = (data) => api.post('/todos', data);
+export const updateTodo = (id, data) => api.put(`/todos/${id}`, data);
+export const deleteTodo = (id) => api.delete(`/todos/${id}`);
 
